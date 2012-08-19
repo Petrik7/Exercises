71e581764dfdc9cd4cb287073ed88220ef6a2946
Petrik mpp <petr_mpp@yahoo.com>
2012-08-18 23:56:47 -0400

    PolindromeKvadrat: Inception

---
+++ PolindromeKvadrat/PolindromeDetector.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PolindromeKvadrat
+{
+    public class MatrixPoint
+    {
+        public int X;
+        public int Y;
+    }
+
+    public class PolindromeDetector
+    {
+        public ICollection<MatrixPoint> GetBiggestSquare(ICollection<string> inputString)
+        {
+            List<MatrixPoint> result = new List<MatrixPoint>();
+
+            return result;
+        }
+    }
+}
---
+++ PolindromeKvadrat/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PolindromeKvadrat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            PolindromeDetector polindromeDetector = new PolindromeDetector();
+
+            List<string> inputStrings = new List<string>();
+
+            List<MatrixPoint> biggestSquare = (List<MatrixPoint>)(polindromeDetector.GetBiggestSquare(inputStrings));
+
+            Console.WriteLine("The biggest polindrome cordinates:");
+            foreach (MatrixPoint point in biggestSquare)
+                Console.WriteLine("X: {0} Y:{1}", point.X, point.Y);
+        }
+    }
+}
